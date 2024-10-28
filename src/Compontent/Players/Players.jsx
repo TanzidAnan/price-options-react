5d1c7816c0346ee59313e2071c77e78ed28904c1
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-28 23:39:13 +0600

    cart section

--- src/Compontent/Players/Players.jsx
+++ src/Compontent/Players/Players.jsx
@@ -1,16 +1,22 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Player from "../player/Player";
 
 
 const Players = () => {
-    const [data,setData] =useState([])
+    const [datas,setDatas] =useState([])
     useEffect(() =>{
         axios.get('Fake.json')
-        .then(data => setData(data.data))
+        .then(data => setDatas(data.data))
     },[])
     return (
         <div className="w-10/12 mx-auto">
-            <h1>Data: {data.length}</h1>
+            <h1>Data: {datas.length}</h1>
+            <div className="grid grid-cols-3 gap-8">
+                {
+                    datas.map((data,idx) =><Player key={idx} data={data}></Player>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Compontent/player/Player.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+
+const Player = ({ data }) => {
+    const { name, img, role, bidding_price } = data
+    console.log(data)
+    return (
+        <div className="">
+            <div className="card bg-base-100 w-96 shadow-xl p-5 gap-6">
+                <figure className="w-[300px] h-[200px] mx-auto">
+                    <img
+                    className="w-full"
+                        src={img}
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <p>{role}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn bg-red-300 text-lg">$ {bidding_price}</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Player;
